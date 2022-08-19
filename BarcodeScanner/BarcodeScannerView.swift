00fddbd3668a31d888794a570e8abbe4e2e07912
Martin Stofko <martinstofko114@gmail.com>
2022-08-19 14:15:18 -0700

    Initial project commit.

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BarcodeScannerView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct BarcodeScannerView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BarcodeScannerView()
     }
 }
